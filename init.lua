d3264f30b3f23cee8f95dfe8b5396f695d444f42
Szendrődi Balázs <balazs.szendrodi23@gmail.com>
2025-02-04 20:57:54 +0100

    quickfix keymaps added

--- init.lua
+++ init.lua
@@ -9,6 +9,9 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua <CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
